a66e7300f0b618631d38c0f15cd3fb41b3f53be8
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 17:25:34 -0600

    Update index.js

--- plugins/netlify-plugin-append-redirects/index.js
+++ plugins/netlify-plugin-append-redirects/index.js
@@ -2,10 +2,16 @@ import fs from 'node:fs';
 
 export const onPostBuild = function({ constants }) {
     fs.readFile('_redirect', 'utf8', (err, data) => {
-        if (err) throw err;
+        if (err) {
+            console.log("Failed to read _redirect")
+            throw err
+        }
         fs.appendFile(`${constants.PUBLISH_DIR}/_redirect`, data, (err) => {
-            if (err) throw err;
-            console.log('Appened _redirect to dist/_redirect');
+            if (err) {
+                console.log("Failed to append _redirect to dist/_redirect")
+                throw err
+            }
+            console.log('Successfully appened _redirect to dist/_redirect');
         });
     });
 }
