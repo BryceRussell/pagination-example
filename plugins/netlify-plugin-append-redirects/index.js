43769178e289b4bbbac53e571b71d9b40fc428fc
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 17:22:10 -0600

    fs test

--- plugins/netlify-plugin-append-redirects/index.js
+++ plugins/netlify-plugin-append-redirects/index.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
 export const onPostBuild = function({ constants }) {
-    console.log(constants.PUBLISH_DIR);
+    fs.readFile('_redirect', 'utf8', (err, data) => {
+        if (err) throw err;
+        fs.appendFile(`${constants.PUBLISH_DIR}/_redirect`, data, (err) => {
+            if (err) throw err;
+            console.log('Appened _redirect to dist/_redirect');
+        });
+    });
 }
