880566e1b850a60d874543c618adf0cd08adaa34
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 21:18:31 -0600

    Update index.js

--- plugins/netlify-plugin-post-script/index.js
+++ plugins/netlify-plugin-post-script/index.js
@@ -1,13 +1,13 @@
 import fs from 'node:fs';
 
-export const onPostBuild = function() {
+export const onPostBuild = function({ constants }) {
     console.log("Attempting to append _redirects to dist/_redirects...")
     fs.readFile('/opt/build/repo/_redirects', 'utf-8', function (err, data) {
         if (err) {
             console.log("Could not read _redirects")
             throw err
         }
-        fs.appendFile('/opt/build/repo/dist/_redirects', '\n' + data, function (err) {
+        fs.appendFile(`/opt/build/repo/${constants.PUBLISH_DIR}/_redirects`, '\n' + data, function (err) {
             if (err) {
                 console.log("Could not append _redirects to dist/_redirects")
                 throw err
