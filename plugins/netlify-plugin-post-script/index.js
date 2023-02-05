4c4c8a7c0faafdf82d802193229d94879576d5c2
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 20:22:10 -0600

    Update index.js

--- plugins/netlify-plugin-post-script/index.js
+++ plugins/netlify-plugin-post-script/index.js
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process';
 
-export const onPostBuild = function() {
+export const onPostBuild = function(options) {
     console.log("netlify-plugin-post-script: Attempting to run 'post-build' script")
+    console.log(JSON.stringify(options))
     execSync(`npm run post-build`);
 }
