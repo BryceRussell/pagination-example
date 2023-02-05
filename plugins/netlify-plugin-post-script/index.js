d896858f5c689dca3ecafa2ca5cbf8c148be3ac2
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 20:43:35 -0600

    test script

--- plugins/netlify-plugin-post-script/index.js
+++ plugins/netlify-plugin-post-script/index.js
@@ -2,15 +2,15 @@ import fs from 'node:fs';
 import { execSync } from 'node:child_process';
 
 export const onPostBuild = function(options) {
-    console.log("Running: netlify-plugin")
-    fs.readFile('/opt/build/repo/_redirect', 'utf-8', function (err, data) {
-        if (err) throw err;
-        console.log("Read _redirect file")
-        fs.appendFile('/opt/build/repo/dist/_redirect', data, function (err) {
-            if (err) throw err;
-            console.log('appended _redirect to dist/_redirect');
-        });
-    });
+    // console.log("Running: netlify-plugin")
+    // fs.readFile('/opt/build/repo/_redirect', 'utf-8', function (err, data) {
+    //     if (err) throw err;
+    //     console.log("Read _redirect file")
+    //     fs.appendFile('/opt/build/repo/dist/_redirect', data, function (err) {
+    //         if (err) throw err;
+    //         console.log('appended _redirect to dist/_redirect');
+    //     });
+    // });
 
     execSync(`npm run post-build`);
 }
